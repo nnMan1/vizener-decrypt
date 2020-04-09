0d297aded96c04c437fcd8b767cac5a884fbdbcf
velibor <dosljakvelibor@gmail.com>
2020-04-09 11:08:01 +0200

    Adds missing files to commit

---
+++ static/js/vizener.js
@@ -0,0 +1,51 @@
+let decrypted = document.querySelector("#decrypted_text")
+let key = document.querySelector("#key_input")
+let encrypted = document.querySelector("#encrypted_text")
+
+function encrypt() {
+
+$.ajax('/encrypt/vizener', {
+    method: 'GET',
+    data: {
+        key: key.value,
+        text: decrypted.value
+    }
+})
+.then(
+    function success(resopnse) {
+        encrypted.value = resopnse
+    },
+
+    function fail(data, status) {
+        alert('Doslo je do greske');
+    }
+);
+}
+
+function decrypt() {
+
+$.ajax('/decrypt/vizener', {
+    method: 'GET',
+    data: {
+        encrypted: encrypted.value
+    }
+})
+.then(
+    function success(response) {
+        decrypted.value = response.decrypted
+        key.value = response.key
+    },
+
+    function fail(data, status) {
+        alert('Doslo je do greske');
+    }
+);
+}
+
+document.querySelector("button").addEventListener("click", e => {
+    if($('input[name=type]:checked').val() == "decrypt") {
+        decrypt()
+    } else {
+        encrypt()
+    }
+})
